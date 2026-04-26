12c505817823245a6c11c3047424b5ca9f734417
Rachid <rachidejjami@gmail.com>
2026-04-26 11:30:59 +0200

    add reset token to user model

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -14,6 +14,7 @@ class User(Base):
     is_active = Column(Boolean, default=True)
     email_verified = Column(Boolean, default=True)
     activation_token = Column(String, nullable=True)
+    reset_token = Column(String, nullable=True)
 
     role = Column(String, default="user")
 
