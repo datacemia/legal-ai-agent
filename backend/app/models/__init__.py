13711ba723cb1e5da937f1bf7c9d39742c448af9
Rachid <rachidejjami@gmail.com>
2026-06-16 22:23:12 +0200

    Update models init

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,6 +1,7 @@
 from app.models.api_key import ApiKey
 from app.models.api_usage import ApiUsage
 from app.models.uploaded_file import UploadedFile
+from app.models.credit_transaction import CreditTransaction
 
 __all__ = [
     "ApiKey",
